e5b7f08287e0f007ef32b5ad38af6c4fbb3cad60
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-03-04 15:27:37 -0600

    Item Handler

--- Project Smash/Assets/_Scripts/Items/ItemHandler.cs
+++ Project Smash/Assets/_Scripts/Items/ItemHandler.cs
@@ -8,7 +8,7 @@ namespace PSmash.Items
     /// <summary>
     /// Will be in charge of receiveng the Inputs, and looking for all the items in the Inventory System.
     /// Send to the UseItem the curren equipped item for the player to use it.
-    /// Send to the UI the 2 items that will be displayed in the correct order.
+    /// Send to the UI the only 2 items that will be displayed in the correct order.
     /// </summary>
     public class ItemHandler : MonoBehaviour
     {
