cab4db38cbde622ca015f14d752e67a1dd257c48
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-03-16 10:58:47 -0600

    Sprint Fixes

---
+++ Project Smash/Assets/_Scripts/SceneManagement/IChangeVCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSmash.SceneManagement
+{
+    public interface IChangeVCamera
+    {
+        void EnterCamArea();
+
+        void ExtiCamArea();
+    }
+
+}
---
+++ Project Smash/Assets/_Scripts/SceneManagement/SceneVirtualCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+namespace PSmash.SceneManagement
+{
+    public class SceneVirtualCamera : MonoBehaviour, IChangeVCamera
+    {
+        [SerializeField]  CinemachineVirtualCamera virtualCamera = null;
+
+        public void EnterCamArea()
+        {
+            virtualCamera.m_Priority = 20;
+        }
+
+        public void ExtiCamArea()
+        {
+            virtualCamera.m_Priority = 0;
+        }
+    }
+}
+
