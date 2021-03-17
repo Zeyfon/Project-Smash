7d306bbdd8b65bd271fd540355cf7ab9cb0fa20f
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-03-16 21:12:44 -0600

    Stalactite Added

---
+++ Project Smash/Assets/Stalactite.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PSmash.Attributes;
+using PSmash.Combat.Weapons;
+
+public class Stalactite : MonoBehaviour, IDamagable
+{
+    [Header("CONFIG")]
+    [SerializeField] bool isDestroyedByMace = false;
+
+    [Header("Material")]
+    [SerializeField] Weapon weaknessWeapon = null;
+    [SerializeField] Sprite destroyedSprite = null;
+    [SerializeField] AudioSource audioSource = null;
+    [SerializeField] AudioClip destroyedClip = null;
+    [SerializeField] AudioClip damagedClip = null;
+
+    
+    public void TakeDamage(Transform attacker, Weapon weapon, float damage)
+    {
+        audioSource.PlayOneShot(damagedClip);
+        if (weapon == this.weaknessWeapon)
+        {
+            if (isDestroyedByMace)
+            {
+                Break();
+            }
+        }
+    }
+
+    void Break()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        GetComponentInChildren<SpriteRenderer>().sprite = destroyedSprite;
+        audioSource.clip = destroyedClip;
+        audioSource.Play();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isDestroyedByMace && collision.collider.GetComponent<Boulder>())
+        {
+            Break();
+        }
+    }
+}
