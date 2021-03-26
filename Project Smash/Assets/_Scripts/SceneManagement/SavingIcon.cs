3d175cb4522dbde1bcd3f85397e186f28440367c
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-03-26 08:45:23 -0600

    Saving Icon was added to the saving process

---
+++ Project Smash/Assets/_Scripts/SceneManagement/SavingIcon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using PSmash.Attributes;
+using PSmash.Core;
+
+namespace PSmash.SceneManagement
+{
+    public class SavingIcon : MonoBehaviour
+    {
+        [SerializeField] CanvasGroup canvasGroup;
+        Coroutine coroutine;
+        public void ShowIcon()
+        {
+            if (coroutine != null) StopCoroutine(coroutine);
+            coroutine = StartCoroutine(ShowingIcon());
+        }
+        public void HideIcon()
+        {
+            if (coroutine != null) StopCoroutine(coroutine);
+            coroutine = StartCoroutine(HidingIcon());
+        }
+
+        IEnumerator ShowingIcon()
+        {
+            print("Showing Saving Icon");
+            Fader fader = new Fader();
+            yield return fader.FadeIn(canvasGroup, 0.5f);
+        }
+
+        IEnumerator HidingIcon()
+        {
+            print("Hiding Saving Icon");
+            Fader fader = new Fader();
+            yield return fader.FadeOut(canvasGroup, 2);
+        }
+    }
+
+}
