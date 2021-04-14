543eefd7f991b0e4d3a5fb3cacf5106f21b87789
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-04-14 12:38:57 -0500

    Smasher + Little stuff were updated

---
+++ Project Smash/Assets/ArmoredEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmoredEnemy : MonoBehaviour
+{
+    [SerializeField] float unarmoredSpeedFactor;
+    [SerializeField] float unarmoredAttackSpeedFactor;
+
+    public float GetSpeedFactorModifier()
+    {
+        return unarmoredSpeedFactor;
+    }
+
+    public float GetAttackSpeedFactor()
+    {
+        return unarmoredAttackSpeedFactor;
+    }
+}
