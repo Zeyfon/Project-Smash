1a0f80bd476535924c8638f57cd99e9532362d11
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-04-27 12:31:07 -0500

    Delete Save File

---
+++ Project Smash/Assets/Editor/DeleteSave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using PSmash.SceneManagement;
+
+public class DeleteSave
+{
+
+    [MenuItem("MyTools/Delete Save File")]
+    public static void DeleteSaveFile()
+    {
+        //GetWindow<DeleteSave>("Example");
+        SavingWrapper[] savingWrappers = Resources.FindObjectsOfTypeAll<SavingWrapper>();
+        savingWrappers[0].Delete();
+    }
+
+}
