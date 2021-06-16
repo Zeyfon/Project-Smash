9033ae61468b7d98555437b90c1cdc821c133a46
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-06-15 20:15:00 -0500

    Movement Rework
    Make it more easily readable

---
+++ Project Smash/Assets/_Scripts/Movement/CharacterMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSmash.Movement
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] protected float baseSpeed;
+        [SerializeField] protected float slopeCheckDistance = 0.5f;
+        [SerializeField] protected LayerMask whatIsGround;
+
+        [Header("PhysicsMaterials")]
+        [SerializeField] protected PhysicsMaterial2D noFriction = null;
+        [SerializeField] protected PhysicsMaterial2D lowFriction = null;
+        [SerializeField] protected PhysicsMaterial2D fullFriction = null;
+
+        protected SlopeControl slope = new SlopeControl();
+        protected Rigidbody2D rb;
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="movementDirection"></param> Must be normalized
+        /// <param name="isGrounded"></param>
+        /// <param name="speedFactor"></param>
+        /// <param name="speedModifier"></param>
+        public virtual void Movement(Vector2 movementDirection, bool isGrounded,float speedFactor, float speedModifier)
+        {
+            float speedModified = baseSpeed * speedFactor * speedModifier;
+            float xVelocity;
+            if (!isGrounded)
+            {
+                rb.sharedMaterial = noFriction;
+                xVelocity = movementDirection.x * speedModified;
+                rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+            }
+            else if (movementDirection.magnitude == 0)
+            {
+                rb.sharedMaterial = lowFriction;
+                rb.velocity = new Vector2(0, 0);
+            }
+            else
+            {
+                rb.sharedMaterial = noFriction;
+                Vector2 directionAfterSlope = slope.GetMovementDirectionWithSlopecontrol(transform.position, movementDirection, slopeCheckDistance, whatIsGround);
+                if (directionAfterSlope.sqrMagnitude == 0)
+                {
+                    //Entering this part means that the slope raycast does not detect any ground and the isGround bool is still true
+                    xVelocity = movementDirection.x * speedModified;// * Mathf.Abs(movementDirectionNormalized.x);
+                    rb.velocity = new Vector2(xVelocity, rb.velocity.y);
+                }
+                else
+                {
+                    xVelocity = directionAfterSlope.x * speedModified;
+                    float yVelocity = directionAfterSlope.y * speedModified;
+                    rb.velocity = new Vector2(xVelocity, yVelocity);
+                    if (!gameObject.CompareTag("Player"))
+                        print(gameObject.name + "  " + rb.velocity);
+                }
+            }
+        }
+    }
+}
+
---
+++ Project Smash/Assets/_Scripts/Movement/LadderMovementControl.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PSmash.Attributes;
+
+namespace PSmash.Movement
+{
+    public class LadderMovementControl
+    {
+        public bool CheckForClimbingLadder(Transform oneWayPlatform, float yInput, bool isGrounded, bool isCollidingWithOneWayPlatform)
+        {
+            //Debug.Log("Looking for Ladder");
+            if (isCollidingWithOneWayPlatform && yInput < -0.9f)
+            {
+                oneWayPlatform.GetComponent<OneWayPlatform>().RotatePlatform();
+                return true;
+            }
+            else if (!isGrounded && Mathf.Abs(yInput) > 0.9f)
+            {
+                Debug.Log("Is not Grounded");
+
+                return true;
+            }
+      
+            else if (isGrounded && !isCollidingWithOneWayPlatform && yInput > 0.8f)
+            {
+                Debug.Log("Is Grounded");
+                return true;
+            }
+            else
+                return false;
+        }
+        /// <summary>
+        /// Sets the player to climb the ladder.
+        /// Used by the LadderClimbingState
+        /// </summary>
+        public void LadderMovementSetup(Transform transform, float ladderPositionX)
+        {
+            Rigidbody2D rb = transform.GetComponent<Rigidbody2D>();
+            rb.position = new Vector3(ladderPositionX, transform.position.y, 0);
+            rb.velocity = new Vector2(0, 0);
+            rb.gravityScale = 0;
+        }
+
+
+        /// <summary>
+        /// The controlled movement on the ladder
+        /// Used by the LadderClimbingState
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="maxLadderMovementSpeed"></param>
+        public void LadderMovement(Vector2 input, float maxLadderMovementSpeed, Transform transform, Vector2 colliderSize, LayerMask whatIsLadderTop, Animator animator, Rigidbody2D rb,   float gravityScale, LayerMask whatIsGround, bool isGrounded, bool isCollidingWithOneWayPlatform, float checkLadderDistance)
+        {
+            animator.SetFloat("climbingSpeed", Mathf.Abs(input.y));
+            rb.velocity = new Vector2(0, input.y * maxLadderMovementSpeed);
+        }
+
+        /// <summary>
+        /// The options for the player to exit the ladder without any additional input
+        /// </summary>
+        /// <param name="yInput"></param>
+        public bool CheckToExitLadder(float yInput, Transform transform, Vector2 colliderSize, LayerMask whatIsLadderTop, Animator animator, Rigidbody2D rb, float gravityScale, LayerMask whatIsGround, bool isGrounded, bool isCollidingWithOneWayPlatform, float checkLadderDistance)
+        {
+            if (yInput > 0 && IsOneWayPlatformBeneathMe(transform, colliderSize, whatIsLadderTop))
+            {        
+                FinishClimbingFromAbove(animator, rb, gravityScale);
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, colliderSize.y / 2, whatIsLadderTop);
+                rb.MovePosition(hit.point);
+                //Debug.Break();
+                return true;
+            }
+
+            else if (yInput < 0 && isGrounded && !isCollidingWithOneWayPlatform)
+            {
+                //print("Exiting Ladder from below");
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, colliderSize.y / 2), Vector2.down, checkLadderDistance, whatIsGround);
+                if (!hit || hit.collider.CompareTag("ThinPlatform"))
+                {
+                    return false;
+                }
+                else
+                {
+                    FinishClimbingFromBelow(animator, rb, gravityScale);
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns true if a One Way Platform is below the player 
+        /// </summary>
+        /// <returns></returns>
+        bool IsOneWayPlatformBeneathMe(Transform transform, Vector2 colliderSize, LayerMask whatIsLadderTop)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, colliderSize.y / 2, whatIsLadderTop);
+            Debug.DrawRay(transform.position, Vector2.down * colliderSize.y / 2,Color.red);
+            if (hit && hit.collider.CompareTag("ThinPlatform"))
+            {
+                return true;
+            }
+            else 
+                return false;
+        }
+
+        /// <summary>
+        /// End Ladder Climbing going back to idle inmediately
+        /// </summary>
+        void FinishClimbingFromBelow(Animator animator, Rigidbody2D rb, float gravityScale)
+        {
+            rb.gravityScale = gravityScale;
+            animator.SetInteger("LadderMovement", 10);
+        }
+
+        /// <summary>
+        /// End Ladder Climbing using the Finish Climbing Ladder Animation
+        /// </summary>
+        void FinishClimbingFromAbove(Animator animator, Rigidbody2D rb, float gravityScale)
+        {
+            rb.velocity = new Vector2(0, 0);
+            animator.SetInteger("LadderMovement", 5);
+        }
+    }
+
+
+}
