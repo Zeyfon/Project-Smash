a941b82646906daeb34024df1762c29392d486e5
Bape <43990665+Zeyfon@users.noreply.github.com>
2021-08-21 11:47:50 -0500

    Collectible Rework

---
+++ Project Smash/Assets/_Scripts/Inventory/CollectibleItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PSmash.Inventories
+{
+    [CreateAssetMenu(menuName = "Items/Collectible Item")]
+    public class CollectibleItem : Item
+    {
+
+    }
+}
